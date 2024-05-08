f0a517341f953eeccdf95b2aae6d3f70c0c2b719
hasherezade <hasherezade@gmail.com>
2024-05-08 15:21:18 -0700

    [FEATURE] Added 32-bit encoder

--- main.cpp
+++ main.cpp
@@ -14,10 +14,25 @@ unsigned char enc_stub64[] = {
     0xE2, 0xF6, // loop decode
 };
 
-void xor_data(void* data, size_t data_size, uint64_t key)
+unsigned char enc_stub32[] = {
+    0xEB, 0x03, //jmp _start
+    0x58, // fetchIP: pop eax
+    0xFF, 0xE0, // jmp eax
+    0x31, 0xC9, // _start: xor ecx, ecx
+    0x81, 0xE9, 0x45, 0xFF, 0xFF, 0xFF, // sub rcx, 0xffffff45 //~(shellcode_size/sizeof(uint64_t)) -> offset: 9
+    0xE8, 0xF0, 0xFF,0xFF, 0xFF, //call fetchIP
+    0xBB, 0x78, 0x56, 0x34, 0x12, // mov ebx, [xor_key] -> offset: 19
+    0x31, 0x58, 0x0D, //decode: xor dword ptr ds:[eax+0xD],ebx
+    0x83, 0xE8, 0xFC, // sub eax,FFFFFFFC
+    0xE2, 0xF8, // loop decode
+};
+//EB 03 58 FF E0 31 C9 81 E9 78 56 34 12 E8 F0 FF FF FF BB 78 56 34 12 31 58 0D 83 E8 FC E2 F8
+
+template <class KEY_TYPE>
+void xor_data(void* data, size_t data_size, KEY_TYPE key)
 {
-    uint64_t* dataq = (uint64_t*)data;
-    size_t sizeq = data_size / sizeof(uint64_t);
+    KEY_TYPE* dataq = (KEY_TYPE*)data;
+    size_t sizeq = data_size / sizeof(KEY_TYPE);
     for (size_t i = 0; i < sizeq; i++) {
         dataq[i] ^= key;
     }
@@ -67,7 +82,8 @@ bool test_shellc(void* exec, size_t exec_size)
     return true;
 }
 
-bool try_xor_data(void* data, size_t data_len, BYTE* payload, size_t payload_size, uint64_t enc_key)
+template <class KEY_TYPE>
+bool try_xor_data(void* data, size_t data_len, BYTE* payload, size_t payload_size, KEY_TYPE enc_key)
 {
     ::memcpy(data, payload, payload_size);
     xor_data(data, data_len, enc_key);
@@ -134,6 +150,45 @@ BYTE* encode_shellc64(BYTE *payload, size_t payload_size, size_t &encoded_size)
     return exec;
 }
 
+BYTE* encode_shellc32(BYTE* payload, size_t payload_size, size_t& encoded_size)
+{
+    uint32_t* shellc_size = (uint32_t*)((ULONG_PTR)enc_stub32 + 9);
+    size_t rounded_len = (payload_size / sizeof(uint64_t));
+    if ((payload_size % sizeof(uint64_t)) > 1) rounded_len++;
+
+    *shellc_size = ~(rounded_len);
+    std::cout << "Shellc Size: 0x" << std::hex << ~(*shellc_size) << " = " << std::dec << ~(*shellc_size) << "\n";
+
+    size_t data_len = rounded_len * sizeof(uint64_t);
+    void* data = VirtualAlloc(0, data_len + 1, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    if (!data) {
+        return nullptr;
+    }
+
+    uint32_t* xor_key = (uint32_t*)((ULONG_PTR)enc_stub32 + 19);
+    uint32_t enc_key = ~(1);
+    srand(time(nullptr));
+    do {
+        enc_key *= rand();
+        enc_key += rand();
+        *xor_key = enc_key;
+#ifdef _DEBUG
+        std::cout << "xor_key: " << std::hex << (*xor_key) << "\n";
+#endif
+    } while (!try_xor_data(data, data_len, payload, payload_size, enc_key));
+
+    std::cout << "xor_key: " << std::hex << (*xor_key) << "\n";
+
+    const size_t stub_size = sizeof(enc_stub32);
+    const size_t exec_size = data_len + stub_size;
+    BYTE* exec = (BYTE*)VirtualAlloc(0, exec_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    encoded_size = exec_size;
+    RtlMoveMemory(exec, enc_stub32, stub_size);
+    RtlMoveMemory((void*)((ULONG_PTR)exec + stub_size), data, data_len);
+    VirtualFree(data, 0, MEM_RELEASE);
+    return exec;
+}
+
 int main(int argc, char *argv[])
 {
     size_t payload_len = 0;
@@ -167,6 +222,8 @@ int main(int argc, char *argv[])
     BYTE* exec = nullptr;
 #ifdef _WIN64
     exec = encode_shellc64(payload, payload_len, exec_size);
+#else
+    exec = encode_shellc32(payload, payload_len, exec_size);
 #endif
     if (!exec) {
         std::cerr << "Encoding failed!\n";
